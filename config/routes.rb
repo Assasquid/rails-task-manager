2d7ae54e30228775fd631d8e157be8dcd8268f3b
Jerome Million Rousseau <jerome@nebula-tales.com>
2023-02-21 16:11:00 +0000

    User can now create a new task.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root'tasks#index'
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
 end
